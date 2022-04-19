e7934954b89eef685ed3bb84493d786d0f13d675
Archana Ingle <103937935+Archana0218@users.noreply.github.com>
2022-04-19 23:44:54 +0530

    Update classobject.java

--- CoreJava/src/java_program/classobject.java
+++ CoreJava/src/java_program/classobject.java
@@ -9,14 +9,14 @@ public class classobject {
 	
 	public void dis()
 	{
-		System.out.println("Year of purchess"+2000);
+		System.out.println("Year of purchase"+2000);
 		System.out.println("make"+"suzuki");
 		System.out.println("Cost of car"+10l);
 	}
 	public void dis2()
 	{
-		System.out.println("Year of purchess"+2022);
-		System.out.println("make"+"maroti");
+		System.out.println("Year of purchase"+2022);
+		System.out.println("make"+"maruti");
 		System.out.println("Cost of car"+2l);
 	}
 	public static void main(String[] args) {
