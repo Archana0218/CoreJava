d5f878689162e18c8f071110661649eb1b915f0c
Dell <Dell@DESKTOP-4LTL1EU>
2022-04-20 21:06:42 +0530

    dataabstraction

--- CoreJava/src/java_program/AtmMachine.java
+++ CoreJava/src/java_program/AtmMachine.java
@@ -1,37 +1,36 @@
 package java_program;
 
-
-class AtmMachine
-{
-	private double atmAmount;
-	public void getAmountinfo()
-	{
-		atmAmount=45000;
-		System.out.println("we have amount in the ATM is: "+atmAmount);
-	}
+public class  AtmMachine{
 	
-}
-
-class MoneyWindrawal extends AtmMachine
-{
-	
-	private double widrwalAmount;
-	public void getAmountinfo()
-	{
-		widrwalAmount=23000;	
-		System.out.println("we have widrawal amount in the ATM is: "+widrwalAmount);
-	}
-	
-}
-
-class AtmTest
-{
-	public static void main(String[] args) {
+		private double atmAmount;
+		public void getAmountinfo()
+		{
+			atmAmount=45000;
+			System.out.println("we have amount in the ATM is: "+atmAmount);
+		}
 		
-		MoneyWindrawal moneyWindrawal=new MoneyWindrawal();
-		moneyWindrawal.getAmountinfo();
 	}
-}
+
+	class MoneyWindrawal extends AtmMachine
+	{
+		
+		private double widrwalAmount;
+		public void getAmountinfo()
+		{
+			widrwalAmount=23000;	
+			System.out.println("we have widrawal amount in the ATM is: "+widrwalAmount);
+		}
+		
+	}
+
+	class AtmTest
+	{
+		public static void main(String[] args) {
+			
+			MoneyWindrawal moneyWindrawal=new MoneyWindrawal();
+			moneyWindrawal.getAmountinfo();
+		}
+	}
 
 
 
