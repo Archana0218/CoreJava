96e5168a5fe0d748ed7e4546e9bc819d156396bf
Archana Ingle <103937935+Archana0218@users.noreply.github.com>
2022-04-25 22:23:58 +0530

    static variable

---
+++ staticVariable.java
@@ -0,0 +1,16 @@
+package java_program;
+
+public class staticVariable {
+	static int a = 10;
+
+	public static void main(String[] args) {
+		System.out.println("sv1.a value:" + staticVariable.a);
+		System.out.println("sv2.a value:" + staticVariable.a);
+
+		// change value of a
+		staticVariable.a = 20;
+		System.out.println("sv1.a value:" + staticVariable.a);
+		System.out.println("sv2.a value: " + staticVariable.a);
+	}
+
+}
